24c317cb6ac22534a04e6b6fdb9911cca6a28677
lizgonye <elizabeth.gonye@richmond.edu>
2016-04-11 18:53:19 -0400

    Update Data_Record.java

--- Data_Record.java
+++ Data_Record.java
@@ -1,8 +1,15 @@
-
+import java.sql.*;
 
 public abstract class Data_Record 
 {
-	String myPrimaryKey;
+	String info;
+	Connection connection;
+	
+	public DataRecord(String s, Connection conn)
+	{
+		this.info = s;
+		this.connection = conn;
+	}
 
 	public abstract void insert(String s);
 	public abstract void update(String s);
