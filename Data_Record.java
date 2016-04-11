d41223a625e820d76b6b2f57061e4cd25832863a
lizgonye <elizabeth.gonye@richmond.edu>
2016-04-11 19:55:04 -0400

    Update Data_Record.java

--- Data_Record.java
+++ Data_Record.java
@@ -2,13 +2,34 @@ import java.sql.*;
 
 public abstract class Data_Record 
 {
-	String info;
-	Connection connection;
+	private String info;
+	private static Connection connection;
 	
-	public DataRecord(String s, Connection conn)
+	public DataRecord(String s)
 	{
 		this.info = s;
-		this.connection = conn;
+	}
+	
+	//Sets up a connection to the database that all the instances of DataRecord can use for their queries need to call this
+	//in the GUI (DataRecord.setUpConnection()) before you can actually do any queries
+	public static void setUpConnection()
+	{
+		try{
+		//The information necessary to create a connection to the database
+		String USR = "";
+		String PWD = "";
+		String URL = "";
+		
+		String driverName = "";
+		Class.forName(driverName);
+		
+		connection = DriverManager.getConnection(URL, USR, PWD);
+		
+		System.out.println("Connected to C325.");
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	public abstract void insert(String s);
