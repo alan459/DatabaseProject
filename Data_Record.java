90b0552d549af7fcefdc654b8669de8305e8ed1c
alan459 <alan.malayev@richmond.edu>
2016-04-11 11:43:43 -0400

    added some basic layout code

--- Data_Record.java
+++ Data_Record.java
@@ -1,4 +1,15 @@
-public class Data_Record 
+
+
+public abstract class Data_Record 
 {
-	
+	String myPrimaryKey;
+
+	public abstract void insert(String s);
+	public abstract void update(String s);
+
+
+	public abstract void search(String primaryKey);
+
+	//public abstract void searchByName(String name);
+
 }
--- Hospital_Frame.java
+++ Hospital_Frame.java
@@ -1,5 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Hospital_Frame {
-	
+public class Hospital_Frame extends JFrame 
+{
+	public static final int WIDTH = 300;
+	public static final int HEIGHT = 300;
+
+	public Hospital_Frame() 
+	{
+		setTitle("Hospital");
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	/**
+	* Fetches the data in the data fields and returns them in a tab-delimited string.
+	* (and checks for correct formatting?)
+	**/
+	public String getDataFields() 
+	{
+
+	}
+
+	/**
+	* Gets and returns the selected relation in the drop down menu
+	**/
+	public String getDataType() 
+	{
+
+	}
+
 }
