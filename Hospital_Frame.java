691e378bb842e7e73079bb95c268b01b439f0c65
alan459 <alan.malayev@richmond.edu>
2016-04-12 15:38:08 -0400

    added some code for the main menu

--- Hospital_Frame.java
+++ Hospital_Frame.java
@@ -12,7 +12,19 @@ public class Hospital_Frame extends JFrame
 		setSize(WIDTH, HEIGHT);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		createMainWindow();
 	}
+
+
+	/**
+	* Creates a jpanel with the frames displaying the main window options/menu.
+	**/
+	public void createMainWindow()
+	{
+		add(new Main_Menu_Panel());
+	}
+
 
 	/**
 	* Fetches the data in the data fields and returns them in a tab-delimited string.
@@ -20,15 +32,45 @@ public class Hospital_Frame extends JFrame
 	**/
 	public String getDataFields() 
 	{
-
+		return "";
 	}
+
 
 	/**
 	* Gets and returns the selected relation in the drop down menu
 	**/
 	public String getDataType() 
 	{
-
+		return "";
 	}
+
+
+	private class Main_Menu_Panel extends JPanel 
+	{
+
+		private JButton patientButton, doctorButton, nurseButton;
+		private JLabel recordSearchLabel;
+
+		public Main_Menu_Panel() 
+		{
+			recordSearchLabel = new JLabel("RECORD Search");
+			patientButton = new JButton("Patient");
+			doctorButton = new JButton("Doctor");
+			nurseButton = new JButton("Nurse");
+
+			add(recordSearchLabel);
+			add(patientButton);
+			add(doctorButton);
+			add(nurseButton);
+
+		}
+	}
+
+
+	public static void main(String[] args)
+	{
+		Hospital_Frame frame = new Hospital_Frame();
+	}
+
 
 }
