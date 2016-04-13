f41a3ee8fe044ff1899ded8d7132fe947c73d009
alan459 <alan.malayev@richmond.edu>
2016-04-13 19:31:41 -0400

    added some patient look stuff

--- Hospital_Frame.java
+++ Hospital_Frame.java
@@ -3,15 +3,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-/***************************************************************************************************************
+/****************************************************************************************************************
 * An interactive interface to interact with a hospital database.
-***************************************************************************************************************/
+****************************************************************************************************************/
 public class Hospital_Frame extends JFrame 
 {
-	public static final int WIDTH = 500;
-	public static final int HEIGHT = 400;
+	public static final int WIDTH = 850;
+	public static final int HEIGHT = 600;
 
 	private JPanel currentScreen;
+
 
 	/*****************************************************************************************************
 	* Main constructor to initialize the frame.
@@ -76,14 +77,29 @@ public class Hospital_Frame extends JFrame
 	* Removes the current screen and sets the current screen to the patient menu.
 	*****************************************************************************************************/
 	public void getPatientMenu()
-	{
+	{	
+		//setSize(WIDTH + 150, HEIGHT + 150);
+		//setLocationRelativeTo(null);	// center the window
 		remove(currentScreen);
-
 		add(currentScreen = new Patient_Menu_Panel()); 
 
 		revalidate();
 		repaint();
 	}
+
+
+	/*****************************************************************************************************
+	* Removes the current screen and sets the current screen to the patient menu.
+	*****************************************************************************************************/
+	public void getPatientLookupMenu()
+	{
+		remove(currentScreen);
+		add(currentScreen = new Patient_Lookup_Panel()); 
+
+		revalidate();
+		repaint();
+	}
+
 
 	/*****************************************************************************************************
 	* Removes the current screen and sets the current screen to the doctor menu.
@@ -113,6 +129,16 @@ public class Hospital_Frame extends JFrame
 
 		repaint();
 	}
+
+
+	/*****************************************************************************************************
+	* Main method to start/run the program.
+	*****************************************************************************************************/
+	public static void main(String[] args)
+	{
+		Hospital_Frame frame = new Hospital_Frame();
+	}
+
 
 
 	/*****************************************************************************************************
@@ -196,12 +222,100 @@ public class Hospital_Frame extends JFrame
 			});
 		}
 
-	}
+	} // end main menu class
+
+
+
 
 	/*****************************************************************************************************
-	* Represents an instance of a patient menu to display on the JFrame.
+	* Represents an instance of a patient menu to displaying to the user the options for interacting with 
+	* the patient relation.
 	*****************************************************************************************************/
 	private class Patient_Menu_Panel extends JPanel 
+	{
+		/* Input submission buttons */
+		private JButton updateInfoButton, recordLookupButton, modifyTreatmentButton, mainMenuButton;	
+
+
+		/*********************************************************************************
+		* Main constructor used for setting up the patient menu options.
+		*********************************************************************************/
+		public Patient_Menu_Panel() 
+		{
+			initializeButtons();
+			addActionListeners();
+
+			// add the components to the panel
+			add(updateInfoButton);
+			add(recordLookupButton);
+			add(modifyTreatmentButton);
+
+			add(mainMenuButton);
+		}
+
+
+		/*********************************************************************************
+		* Initializes the buttons for the patient menu.
+		*********************************************************************************/
+		private void initializeButtons()
+		{
+			updateInfoButton = new JButton("UPDATE PATIENT INFO");
+			recordLookupButton = new JButton("PATIENT RECORD LOOKUP");
+			modifyTreatmentButton = new JButton("MODIFY TREATMENT PLAN");
+
+			mainMenuButton = new JButton("Main Menu");
+		}
+
+
+		/*********************************************************************************
+		* Adds the action listeners for the patient menu.
+		*********************************************************************************/
+		private void addActionListeners()
+		{
+			/* Add functionality - Take user to update patient interface */
+			updateInfoButton.addActionListener(new ActionListener()
+			{
+			  public void actionPerformed(ActionEvent e)
+			  {
+			    	
+			  }
+			});
+
+			/* Add functionality - Take user to patient record lookup interface */
+			recordLookupButton.addActionListener(new ActionListener()
+			{
+			  public void actionPerformed(ActionEvent e)
+			  {
+			  	getPatientLookupMenu();
+			  }
+			});
+
+			/* Add functionality - Take user to modofy treatment plan interface */
+			modifyTreatmentButton.addActionListener(new ActionListener()
+			{
+			  public void actionPerformed(ActionEvent e)
+			  {
+			  }
+			});
+
+			/* Add functionality - Return user to main menu */
+			mainMenuButton.addActionListener(new ActionListener()
+			{
+			  public void actionPerformed(ActionEvent e)
+			  {
+			  	getMainMenu();
+			  }
+			});
+		}
+
+	} // end patient menu class
+
+
+
+	/*****************************************************************************************************
+	* A display for the user to lookup patients in the database.
+	*****************************************************************************************************/
+	private class Patient_Lookup_Panel extends JPanel 
 	{
 		/* Input submission buttons */
 		private JButton ssnLookupButton, infoLookupButton, recordLookupButton, mainMenuButton;	
@@ -210,54 +324,66 @@ public class Hospital_Frame extends JFrame
 		private JLabel patientLookupLabel, recordLookupLabel;	
 
 		/* Display to the user what type of information is being displayed in the adjacent position */
-		private JLabel enterSSNLabel, patientInfoLabel;	
+		private JLabel enterSSNLabel, enterFirstNameLabel, enterLastNameLabel, enterDOBLabel, patientInfoLabel;	
 
 		/* Labels for displaying output data */
 		private JLabel patientNameOutputLabel, patientSSNOutputLabel, patientDOBOutputLabel;
 
 		/* Fields for getting input from the user */
-		private JTextField ssnLookupInput;	
+		private JTextField ssnLookupInput, firstNameInput, lastNameInput, dobInput;	
 
 
 		/*********************************************************************************
-		* Represents an instance of a main menu to display on the JFrame.
+		* Main constructor for setting up the patient lookup menu. 
 		*********************************************************************************/
-		public Patient_Menu_Panel() 
+		public Patient_Lookup_Panel() 
 		{
 			initializeLabels();
-
 			initializeButtons();
-
 			initializeTextFields();
-
 			addActionListeners();
 
 			// add the components to the panel
 			add(patientLookupLabel);
-			add(recordLookupLabel);
-
+			//add(recordLookupLabel);
 			add(enterSSNLabel);
+			add(ssnLookupInput);
 			add(ssnLookupButton);
+
+			add(enterFirstNameLabel);
+			add(firstNameInput);
+
+			add(enterLastNameLabel);
+			add(lastNameInput);
+
+			add(enterDOBLabel);
+			add(dobInput);
+
 			add(infoLookupButton);
-			add(recordLookupButton);
+			//add(recordLookupButton);
 			add(mainMenuButton);
 		}
 
 
 		/*********************************************************************************
-		* Initializes the labels for this panel.
+		* Initializes the labels for the Patient lookup menu.
 		*********************************************************************************/
-		private void intializeLabels()
+		private void initializeLabels()
 		{
 			patientLookupLabel = new JLabel("Patient Lookup");
 			recordLookupLabel = new JLabel("Record Lookup");
 
-			enterSSNLabel = new JLabel("Enter patient ssn: ");
+			enterSSNLabel = new JLabel("by ssn:");
+
+			enterFirstNameLabel = new JLabel("by first:");
+			enterLastNameLabel = new JLabel("by last:");
+
+			enterDOBLabel = new JLabel("dob:");
 		}
 
 
 		/*********************************************************************************
-		* Initializes the buttons for this panel.
+		* Initializes the buttons for the patient lookup menu.
 		*********************************************************************************/
 		private void initializeButtons()
 		{
@@ -269,20 +395,23 @@ public class Hospital_Frame extends JFrame
 
 
 		/*********************************************************************************
-		* Initializes the textfields for this panel.
+		* Initializes the textfields for the patient lookup menu.
 		*********************************************************************************/
 		private void initializeTextFields()
 		{
 			ssnLookupInput = new JTextField(10);
+			firstNameInput = new JTextField(10);
+			lastNameInput = new JTextField(10);
+			dobInput = new JTextField(10);
 		}
 
 
 		/*********************************************************************************
-		* Adds the action listeners for the patient menu.
+		* Adds the action listeners for the patient lookup menu.
 		*********************************************************************************/
 		private void addActionListeners()
 		{
-			// lookup a patient based on ssn
+			/* Add functionality - Lookup a patient based on ssn */
 			ssnLookupButton.addActionListener(new ActionListener()
 			{
 			  public void actionPerformed(ActionEvent e)
@@ -291,8 +420,8 @@ public class Hospital_Frame extends JFrame
 			    	if (ssn != null)
 			    	{
 			    		// pass the ssn data to a method that lookups patient data and returns it as a string
-			    		String patientData = getPatientData(ssn);
-			    		displayPatientData(patientData); 
+			    		// String patientData = getPatientData(ssn);
+			    		//displayPatientData(patientData); 
 			    	}
 			    	else
 			    	{
@@ -301,7 +430,7 @@ public class Hospital_Frame extends JFrame
 			  }
 			});
 
-			// return to main menu option
+			/* Add functionality - Return to main menu option */
 			mainMenuButton.addActionListener(new ActionListener()
 			{
 			  public void actionPerformed(ActionEvent e)
@@ -311,17 +440,22 @@ public class Hospital_Frame extends JFrame
 			});
 		}
 
+
 		/*********************************************************************************
 		* Returns the input in the "lookup patient by ssn" field.
 		*********************************************************************************/
 		private String getSSNData()
 		{
-			try {
+			try 
+			{
 				return ssnLookupInput.getText();
-			} catch (Exception e) {
+			} 
+			catch (Exception e) 
+			{
 				return null;
 			}
 		}
+
 
 		/*********************************************************************************
 		* Fills the patient search fields with a patient's data.
@@ -331,15 +465,7 @@ public class Hospital_Frame extends JFrame
 
 		}
 
-	}
-
-	/*****************************************************************************************************
-	* Main method to start/run the program.
-	*****************************************************************************************************/
-	public static void main(String[] args)
-	{
-		Hospital_Frame frame = new Hospital_Frame();
-	}
+	} // end patient lookup class
 
 
 }
