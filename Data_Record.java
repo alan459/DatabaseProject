664d20466708197582d1140846849e917e813c28
lizgonye <elizabeth.gonye@richmond.edu>
2016-04-13 10:51:36 -0400

    Update Data_Record.java

--- Data_Record.java
+++ Data_Record.java
@@ -2,8 +2,8 @@ import java.sql.*;
 
 public abstract class Data_Record 
 {
-	private String info;
-	private static Connection connection;
+	private String info; //A string to hold the information passed to instance to be used in the query
+	private static Connection connection; //One database connection for all the instances to use to execute queries
 	
 	public DataRecord(String s)
 	{
@@ -15,7 +15,7 @@ public abstract class Data_Record
 	public static void setUpConnection()
 	{
 		try{
-		//The information necessary to create a connection to the database
+		//The information necessary to create a connection to the database, need to figure out what this will be
 		String USR = "";
 		String PWD = "";
 		String URL = "";
@@ -36,7 +36,7 @@ public abstract class Data_Record
 	public abstract void update(String s);
 
 
-	public abstract void search(String s);
+	public abstract void search(String searchType);
 
 	//public abstract void searchByName(String name);
 
