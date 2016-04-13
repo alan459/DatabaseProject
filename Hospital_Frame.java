17041f67f22cfaf5806199b61a2910118762b16c
alan459 <alan.malayev@richmond.edu>
2016-04-13 12:33:31 -0400

    added to the patient menu

--- Hospital_Frame.java
+++ Hospital_Frame.java
@@ -23,7 +23,7 @@ public class Hospital_Frame extends JFrame
 
 
 	/**
-	* Creates a jpanel with the frames displaying the main window options/menu.
+	* Creates a jpanel with the frames displaying the main window options/menu and sets it as the main screen.
 	**/
 	public void createMainWindow()
 	{
@@ -49,17 +49,38 @@ public class Hospital_Frame extends JFrame
 		return "";
 	}
 
+
+	/**
+	* Removes the current screen and sets the current screen to the main menu.
+	**/
+	public void getMainMenu()
+	{
+		remove(currentScreen);
+
+		add(currentScreen = new Main_Menu_Panel()); 
+
+		revalidate();
+
+		repaint();
+	}
+
+
+	/**
+	* Removes the current screen and sets the current screen to the patient menu.
+	**/
 	public void getPatientMenu()
 	{
 		remove(currentScreen);
 
-		add(new Patient_Menu_Panel()); 
+		add(currentScreen = new Patient_Menu_Panel()); 
 
 		revalidate();
 		repaint();
 	}
 
-
+	/**
+	* Removes the current screen and sets the current screen to the doctor menu.
+	**/
 	public void getDoctorMenu()
 	{
 		remove(currentScreen);
@@ -67,10 +88,14 @@ public class Hospital_Frame extends JFrame
 		add(new Patient_Menu_Panel()); 
 
 		revalidate();
+
 		repaint();
 	}
 
 
+	/**
+	* Removes the current screen and sets the current screen to the nurse menu.
+	**/
 	public void getNurseMenu()
 	{
 		remove(currentScreen);
@@ -78,9 +103,14 @@ public class Hospital_Frame extends JFrame
 		add(new Patient_Menu_Panel()); 
 
 		revalidate();
+
 		repaint();
 	}
 
+
+	/**
+	* Represents an instance of a main menu to display on the JFrame.
+	**/
 	private class Main_Menu_Panel extends JPanel 
 	{
 
@@ -89,12 +119,34 @@ public class Hospital_Frame extends JFrame
 
 		public Main_Menu_Panel() 
 		{
-			recordSearchLabel = new JLabel("Record Search");
+			intializeLabels();
 
+			initializeButtons();
+
+			addActionListeners();
+
+			add(recordSearchLabel);
+			add(patientButton);
+			add(doctorButton);
+			add(nurseButton);
+
+		}
+
+
+		private void intializeLabels()
+		{
+			recordSearchLabel = new JLabel("Record Search");
+		}
+
+		private void initializeButtons()
+		{
 			patientButton = new JButton("Patient");
 			doctorButton = new JButton("Doctor");
 			nurseButton = new JButton("Nurse");
+		}
 
+		private void addActionListeners()
+		{
 			patientButton.addActionListener(new ActionListener()
 			{
 			  public void actionPerformed(ActionEvent e)
@@ -118,48 +170,105 @@ public class Hospital_Frame extends JFrame
 			    //getPatientMenu();			    
 			  }
 			});
-
-
-			add(recordSearchLabel);
-			add(patientButton);
-			add(doctorButton);
-			add(nurseButton);
-
 		}
+
 	}
 
 
 	private class Patient_Menu_Panel extends JPanel 
 	{
-
-		private JButton ssnLookupButton, infoLookupButton, recordLookupButton;
+		private JButton ssnLookupButton, infoLookupButton, recordLookupButton, mainMenuButton;
 		private JLabel patientLookupLabel, recordLookupLabel;
+		private JLabel enterSSNLabel;
+		private JTextField ssnLookupInput;
+
 
 		public Patient_Menu_Panel() 
+		{
+			initializeLabels();
+
+			initializeButtons();
+
+			initializeTextFields();
+
+			addActionListeners();
+
+			// add the components to the panel
+			add(patientLookupLabel);
+			add(recordLookupLabel);
+
+			add(enterSSNLabel);
+			add(ssnLookupButton);
+			add(infoLookupButton);
+			add(recordLookupButton);
+			add(mainMenuButton);
+		}
+
+
+		private void intializeLabels()
 		{
 			patientLookupLabel = new JLabel("Patient Lookup");
 			recordLookupLabel = new JLabel("Record Lookup");
 
+			enterSSNLabel = new JLabel("Enter patient ssn: ");
+		}
+
+
+		private void initializeButtons()
+		{
 			ssnLookupButton = new JButton("Submit");
 			infoLookupButton = new JButton("Submit");
 			recordLookupButton = new JButton("Submit");
+			mainMenuButton = new JButton("Main Menu");
+		}
 
+
+		private void initializeTextFields()
+		{
+			ssnLookupInput = new JTextField(10);
+		}
+
+
+		private void addActionListeners()
+		{
 			ssnLookupButton.addActionListener(new ActionListener()
 			{
 			  public void actionPerformed(ActionEvent e)
 			  {
-			    		    
+			    	String ssn = getSSNData();
+			    	if (ssn != null)
+			    	{
+			    		// pass the ssn data to a method that lookups patient data and returns it as a string
+			    		String patientData = getPatientData(ssn);
+			    		displayPatientData(patientData); 
+			    	}
+			    	else
+			    	{
+			    		// display error message somewhere
+			    	}
 			  }
 			});
 
 
-			add(patientLookupLabel);
-			add(recordLookupLabel);
-			add(ssnLookupButton);
-			add(infoLookupButton);
-			add(recordLookupButton);
-
+			mainMenuButton.addActionListener(new ActionListener()
+			{
+			  public void actionPerformed(ActionEvent e)
+			  {
+			   		getMainMenu();
+			  }
+			});
 		}
+
+
+		private String getSSNData()
+		{
+			try {
+				return ssnLookupInput.getText();
+			} catch (Exception e) {
+				return null;
+			}
+		}
+
 	}
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DatabaseProject
+
+Need to know which methodgets passed a patient ssn and returns a string containing the data and the format of that data. 
