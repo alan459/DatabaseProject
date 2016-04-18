e1f12ed409f057f72de25e9dbd56422af5d37834
lizgonye <elizabeth.gonye@richmond.edu>
2016-04-18 11:11:42 -0400

    Update and rename Data_Record.java to DataRecord.java

--- DataRecord.java
+++ DataRecord.java
@@ -1,9 +1,12 @@
 import java.sql.*;
 
-public abstract class Data_Record 
+public abstract class DataRecord 
 {
-	private String info; //A string to hold the information passed to instance to be used in the query
-	private static Connection connection; //One database connection for all the instances to use to execute queries
+	public String info; //A string to hold the information passed to instance to be used in the query
+	public static Connection conn; //One database connection for all the instances to use to execute queries
+	static final String URL = "jdbc:oracle:thin:@abacus1:1525:C325";     
+	static final String USR = "sl7dz";  
+	static final String PWD = "kookaburra";
 	
 	public DataRecord(String s)
 	{
@@ -16,15 +19,13 @@ public abstract class Data_Record
 	{
 		try{
 			//The information necessary to create a connection to the database, need to figure out what this will be
-			static final String URL = "jdbc:oracle:thin:@abacus1:1525:C325";     
-			static final String USR = "sl7dz";  
-			static final String PWD = "kookaburra";
+	
 		
 			// Register driver by finding the class that corresponds do it             
 			String driverName = "oracle.jdbc.OracleDriver"; 
 			Class.forName(driverName);
 		
-			connection = DriverManager.getConnection(URL, USR, PWD);
+			conn = DriverManager.getConnection(URL, USR, PWD);
 		
 			System.out.println("Connected to C325.");
 		}catch(Exception e)
@@ -37,7 +38,7 @@ public abstract class Data_Record
 	public abstract void update(String s);
 
 
-	public abstract void search(String searchType);
+	public abstract String search(String searchType);
 
 	//public abstract void searchByName(String name);
 
